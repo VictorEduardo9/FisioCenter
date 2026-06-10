be7ad5078472f487a19d2f5a97e0b4bffdb6d29c
VictorEduardo9 <victoreduardo9234@gmail.com>
2026-06-09 23:23:02 -0300

    Fase Testes 1

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("btntrocar1").addEventListener("click", () => {
+    const aba1 = document.getElementById("aba1");
+    const aba2 = document.getElementById("aba2");
+
+    if (aba2.style.display === "none") {
+        aba1.style.display = "none";
+        aba2.style.display = "flex";
+    } else {
+        aba1.style.display = "flex";
+        aba2.style.display = "none";
+    }
+
+    
+});
+
+document.getElementById("btntrocar2").addEventListener("click", () => {
+    const aba1 = document.getElementById("aba1");
+    const aba2 = document.getElementById("aba2");
+
+    if (aba2.style.display === "none") {
+        aba1.style.display = "none";
+        aba2.style.display = "flex";
+    } else {
+        aba1.style.display = "flex";
+        aba2.style.display = "none";
+    }
+
+    
+});
